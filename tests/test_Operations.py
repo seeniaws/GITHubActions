9bbed29492f69aa9d84fea43e9da70f28dc359a9
Seenivasan Veerabahu <69489569+seeniaws@users.noreply.github.com>
2025-06-10 10:50:15 -0500

    Update and rename testOperations.py to test_Operations.py

--- tests/test_Operations.py
+++ tests/test_Operations.py
@@ -1,8 +1,8 @@
 from src.mathOperations import add, sub
-def testAdd():
+def test_Add():
     assert add(2,3) == 5    
     assert add(-1,1) == 0
-def testSub():    
+def test_Sub():    
     assert sub(2,3) ==-1
     assert sub(3,2) == 1
     
